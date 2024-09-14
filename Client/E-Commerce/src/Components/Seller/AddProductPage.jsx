d74503b8771ba3d22309ddb637208fd9ce4a3289
Harsha A K <harshaak2003@gmail.com>
2024-09-14 22:39:20 +0530

    btn change

--- Client/E-Commerce/src/Components/Seller/AddProductPage.jsx
+++ Client/E-Commerce/src/Components/Seller/AddProductPage.jsx
@@ -168,7 +168,7 @@ function AddProductPage() {
                                 type="submit"
                                 className="btn mt-12 flex w-[84%] ml-7 items-center justify-center px-4 py-2 font-semibold text-white bg-blue-500 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
                             >
-                                Sign Up
+                                Add Product
                             </button>
                         </Form>
                     )}
