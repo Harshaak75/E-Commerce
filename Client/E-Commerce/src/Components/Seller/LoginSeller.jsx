d4d8238c4d8af241048d88ad3149f50e43e0696a
Harsha A K <harshaak2003@gmail.com>
2024-09-14 20:01:45 +0530

    mm

--- Client/E-Commerce/src/Components/Seller/LoginSeller.jsx
+++ Client/E-Commerce/src/Components/Seller/LoginSeller.jsx
@@ -14,7 +14,7 @@ function LoginSeller() {
     const login = async (values, actions) => {
 
         try {
-            await axios.post("https://e-commerce-backend-1k7q.onrender.com/api/user/loginSeller", {
+            await axios.post("https://e-commerce-frontend-klxu.onrender.com/api/user/loginSeller", {
                 email: values.email,
                 password: values.password
             }, {
