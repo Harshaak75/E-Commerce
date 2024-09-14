55a3ddcc41cf314058b35b464330cb5d1839dbd1
Harsha A K <harshaak2003@gmail.com>
2024-09-14 22:14:40 +0530

    home

--- Client/E-Commerce/src/Components/Seller/AddProducts.jsx
+++ Client/E-Commerce/src/Components/Seller/AddProducts.jsx
@@ -34,6 +34,7 @@ function AddProducts({ products }) {
     try {
       await axios.delete(`https://e-commerce-backend-1k7q.onrender.com/api/user/DeleteProduct/${product}`);
       window.location.reload();
+      navigate("/Home")
     } catch (error) {
       console.log(error)
     }
