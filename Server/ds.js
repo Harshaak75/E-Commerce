e6de8cf7745fcda21e67a594fb99e280710d689b
Harsha A K <harshaak2003@gmail.com>
2024-09-14 18:17:57 +0530

    dbs

--- Server/ds.js
+++ Server/ds.js
@@ -8,11 +8,11 @@ dotenv.config();
 
 
 const db = new pg.Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    user: "postgres",
+    host: "localhost",
+    database: "Mavex",
+    password:"harsha123",
+    port: 5432,
   });
   
   db.connect()
